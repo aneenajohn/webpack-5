d14658a98f66045fda1953bb1ffd40c2b78681ae
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-15 18:29:03 +0530

    Added babel configs

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,22 @@ const webpackConfig = {
 		path: path.resolve(__dirname, "dist")
 	},
 
+
+    module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			}
+		]
+	},
+    
 	mode: "production"
 };
 
